e5207d10e745ea74bbc91427c9223d18f2017406
Davi <davi.aug@protonmail.com>
2020-09-01 16:08:13 -0300

    plasma ambient and applications setup

--- profiledef.sh
+++ profiledef.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="archlinux"
-iso_label="ARCH_$(date +%Y%m)"
-iso_publisher="Arch Linux <https://www.archlinux.org>"
+iso_name="plasmarchiso"
+iso_label="plasmarch_$(date +%Y%m)"
+iso_publisher="Davi Augusto <https://agst.dev>"
 iso_application="Arch Linux Live/Rescue CD"
 iso_version="$(date +%Y.%m.%d)"
 install_dir="arch"
