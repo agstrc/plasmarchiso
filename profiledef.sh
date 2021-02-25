e2f46f81d727cdd6019a060afb2b0562373b35a1
Davi <davi@agst.dev>
2021-02-24 22:43:15 -0300

    Updated releng profile

--- profiledef.sh
+++ profiledef.sh
@@ -2,11 +2,20 @@
 # shellcheck disable=SC2034
 
 iso_name="plasmarchiso"
-iso_label="plasmarch_$(date +%Y%m)"
-iso_publisher="Davi Augusto <https://agst.dev>"
-iso_application="Arch Linux Live/Rescue CD"
+iso_label="PLASMARCH_$(date +%Y%m)"
+iso_publisher="Davi <https://agst.dev>"
+iso_application="Plasma Arch Linux Live/Rescue CD"
 iso_version="$(date +%Y.%m.%d)"
 install_dir="arch"
 bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
 arch="x86_64"
 pacman_conf="pacman.conf"
+airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
+file_permissions=(
+  ["/etc/shadow"]="0:0:400"
+  ["/root"]="0:0:750"
+  ["/root/.automated_script.sh"]="0:0:755"
+  ["/usr/local/bin/choose-mirror"]="0:0:755"
+  ["/usr/local/bin/Installation_guide"]="0:0:755"
+  ["/usr/local/bin/livecd-sound"]="0:0:755"
+)
